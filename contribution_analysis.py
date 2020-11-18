42e258f7673c661ca63082e1a10cf88a3825d847
Elias Pajares <elias.pajares@tum.de>
2020-11-18 15:30:17 +0100

    restructured produced files and added delete

--- contribution_analysis.py
+++ contribution_analysis.py
@@ -53,23 +53,14 @@ def parse_geom(geom_xml,geom_type,modification_type):
         
     return coordinates, dist, geometry
 
-osm_mappers = list(dict.fromkeys(contributors))
-
-f_path = 'changesets/polygons.xml'
-geom_type = 'polygon'
-
-#def mapping_analysis(osm_mappers, f_path, geom_type):
-xml_file = open_xml(f_path)
- 
-type_translation = {"point":"node","line":"way","polygon":"way"}
-
-arr_features = []
-
-
-def contributions_by_mapper(mapper, geom_type, xml_file):  
+def contributions_by_mapper(geoms_features, mapper, geom_type, xml_file):  
     #All modified features will be analyzed
     list_modified_keys = []
     count_modified = 0
+    count_deleted = 0
+    count_created = 0
+    total_length = 0
+    type_translation = {"point":"node","line":"way","polygon":"way"}
     
     feature_type = type_translation[geom_type]
     
@@ -80,7 +71,7 @@ def contributions_by_mapper(mapper, geom_type, xml_file):
             print('Cannot analyze this feature!')
             continue
         
-        if new_feature.attrib['user'] in mapper:
+        if new_feature.attrib['user'] == mapper:
             count_modified += 1
             old_feature = feature.find("./old/"+feature_type)
      
@@ -92,7 +83,7 @@ def contributions_by_mapper(mapper, geom_type, xml_file):
                
             diff_attrib = diff_attributes(attributes_old,attributes_new)
             
-            arr_features.append(geom_new[2])
+            geoms_features.append(geom_new[2])
             
             if geom_old[0] != geom_new[0]:       
                 diff_attrib.append('geometry') 
@@ -103,38 +94,65 @@ def contributions_by_mapper(mapper, geom_type, xml_file):
     d = {"attributes":list_modified_keys}
     df_attrib = pd.DataFrame(data=d)
     
+    for feature in xml_file.findall("./*[@type='delete']"):
+    
+        if feature.find("./old/"+feature_type).attrib['user'] == None:
+            print('Cannot analyze this feature!')
+            continue
+        if feature.find("./old/"+feature_type).attrib['user'] == mapper:
+            count_deleted += 1
+    
+    
     if not df_attrib.empty:
-        df_attrib.groupby('attributes')['attributes'].value_counts().to_csv('results/'+geom_type+'/'+mapper+'_modified_analysis.csv')
         
-        with open('results/'+geom_type+'/'+mapper+'_modified_analysis.csv','a') as fd:
-            fd.write('modified_features,modified_features,'+str(count_modified))
-        
+        df_attrib = df_attrib.groupby('attributes').size().to_frame()
+        df_attrib['Mapper'] = mapper
+        df_attrib.to_csv('results/'+geom_type+'_changed_attributes.csv',mode='a',header=False)
+             
         #All created features will be analyzed
-        count_created = 0
-        total_length = 0
+        
         for feature in xml_file.findall("./*[@type='create']"):
             new_feature = feature.find("./"+feature_type)  
             if new_feature == None:
                 print('Cannot analyze this feature!')
                 continue
-            if new_feature.attrib['user'] in mapper:
+            if new_feature.attrib['user'] == mapper:
                 count_created += 1
                 geom_new = parse_geom(new_feature,geom_type,'new')
                 dist = geom_new[1]
-                arr_features.append(geom_new[2])
+                geoms_features.append(geom_new[2])
                 total_length = total_length + dist
         
-        with open('results/'+geom_type+'/'+mapper+'_modified_analysis.csv','a') as fd:
-            fd.write('\ncreated_features,'+str(count_created))
-            fd.write('\ntotal_size,'+str(total_length))
+               
+        
+    with open('results/'+geom_type+'_count_features.csv','a') as fd:
+        fd.write(mapper+',modified_features,'+str(count_modified)+'\n')
+        fd.write(mapper+',deleted_features,'+str(count_deleted)+'\n')
+        fd.write(mapper+',created_features,'+str(count_created)+'\n')
+        fd.write(mapper+',total_size,'+str(total_length)+'\n')
 
-    return arr_features
+    return geoms_features
+
+
+osm_mappers = list(dict.fromkeys(contributors))
+
+for geom_type in ['point','line','polygon']:
+    f_path = 'changesets/%s.xml' % geom_type
+    xml_file = open_xml(f_path)
+
+    geoms_features = []
     
-for mapper in osm_mappers:
-    features = contributions_by_mapper(mapper, geom_type, xml_file)
-
-with open('results/'+geom_type+'/'+'geometries.geojson','a') as geoms: 
-    geoms.write(str(FeatureCollection(features)))
+    with open('results/'+geom_type+'_changed_attributes.csv','w') as fd: 
+        fd.write('attribute,count,mapper\n')
+        
+    with open('results/'+geom_type+'_count_features.csv','w') as fd: 
+        fd.write('mapper,edit_type,count\n')    
+        
+    for mapper in osm_mappers:
+        geoms_features = contributions_by_mapper(geoms_features, mapper, geom_type, xml_file)
+    
+    with open('results/'+geom_type+'_geometries.geojson','w') as geoms: 
+        geoms.write(str(FeatureCollection(geoms_features)))
     
 
 
